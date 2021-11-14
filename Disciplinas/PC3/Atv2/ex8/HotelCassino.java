017a64910f75ae9e459bf5e885555abbe3b81f3e
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2021-11-14 18:43:31 -0300

    Atividade de PC3

---
+++ Disciplinas/PC3/Atv2/ex8/HotelCassino.java
@@ -0,0 +1,12 @@
+package projetos;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "hotel_cassino")
+
+public class HotelCassino implements Serializable {
+	private static final long serialVersionUID = 1L;
+}
